9a77743c23c3611d6cc62094628d39431cd1a1f0
Glitchii <hello@tempfile.site>
2021-12-17 19:45:21 +0000

    Change sh to bash

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 Changes terminal ps1 to a rickroll after each command.
 
 ###  
-The first time after cloning, cd into `rickroll-terminal` and  run `sh setup.sh`. Run this only once after cloning the repo.
+The first time after cloning, cd into `rickroll-terminal` and  run `bash setup.sh`. Run this only once after cloning the repo.
   
   
-When done, you can now open a new terminal or restart the current terminal session and type `rick` or `rick -r` to reset
+When done, you can now open a new terminal or restart the current terminal session and type `roll` or `roll -r` to reset
--- setup.sh
+++ setup.sh
@@ -2,14 +2,16 @@ mkdir -p ~/.local/bin
 tail -n 59 rick.txt >~/.local/bin/rick && chmod +x ~/.local/bin/rick
 rc=~/.$(basename $SHELL)rc
 
-echo "# Inserted by rickroll-terminal" >> $rc
+echo -e "\n\n# Inserted by rickroll-terminal" >> $rc
 if [[ ! "$PATH" =~ "$HOME/.local/bin" ]]; then
     echo -e '\nexport PATH="$HOME/.local/bin:$PATH"' >> $rc
 fi
 
-tail -n 22 roll.txt >>$rc
+tail -n 20 roll.txt >>$rc
 echo "# End of rickroll-terminal" >> $rc
 
 # if [[ ! "$PATH" =~ "$HOME/.local/bin" ]]; then
 #     export PATH="$HOME/.local/bin:$PATH"
 # fi
+
+echo -e "\e[32mDone, you can now open a new terminal and type 'roll'\e[0m"
